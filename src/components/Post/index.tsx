ba7a422cb2e9b5b511e97947486c674dc5a83c5b
Emerson Pereira da Silva <pereiradasilva.emerson@gmailcom>
2021-01-16 22:35:57 -0300

    Add Buttons Post

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,23 +1,38 @@
 import React from 'react';
+import Avatar from './img_avatar.png'
+
 import './styles.css';
 
 const Post: React.FC = () => {
   return (
     <div className="post-card-container">
       <header className="post-card-header">
-        <h1>Como programar em React</h1>
+        <div>
+          <img className="post-card-header-avatar" src={Avatar} alt="Avatar" />
+        </div>
+        <div>
+          <h1>Como programar em Node, React e React Native</h1>
+          <h5>por Fulano de Tal há 3 dias </h5>
+        </div>
       </header>
       <div className="post-card-description">
         <p>Ferramentas usadas são as melhores</p>
         <p>
           {' '}
-          id: ObjectID; title: string; image_url: string; body: string; author:
-          OmitedUser; users_liked: MongoObjectID[]; category: Category; tags:
-          Tag[]; slug: string; created_at: Date; updated_at: Date;
+          Declarativo
+
+          React faz com que a criação de UIs interativas seja uma tarefa fácil. Crie views simples para cada estado na sua aplicação, e o React irá atualizar e renderizar de forma eficiente apenas os componentes necessários na medida em que os dados mudam.
+          Views declarativas fazem com que seu código seja mais previsível e simples de depurar.
+
         </p>
       </div>
       <footer className="post-card-footer">
-        <h3>link</h3>
+        {' '}
+        <div className="post-card-footer-button" data-toggle="tooltip" title="Gostei">🎁</div>
+        <div className="post-card-footer-button " data-toggle="tooltip" title="Precisa melhorar">🥺</div>
+        <div className="post-card-footer-button " data-toggle="tooltip" title="Denunciar">🕵️‍♂️</div>
+        <div className="post-card-footer-button " data-toggle="tooltip" title="Ler na integra">🔎</div>
+        <div className="post-card-footer-button " data-toggle="tooltip" title="Editar">✍</div>
       </footer>
     </div>
   );
