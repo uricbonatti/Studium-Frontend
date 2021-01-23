59ef22090ebd1dbff2be1e6e3dcb13e4e533df63
Emerson Pereira da Silva <pereiradasilva.emerson@gmailcom>
2021-01-23 04:02:25 -0300

    Add .env.example

--- src/components/PostList/index.tsx
+++ src/components/PostList/index.tsx
@@ -1,23 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './styles.css';
 import Post from '../Post';
+import { useQuery, gql } from '@apollo/client'
+import { GET_POSTS } from '../../graphql/Post/ListPostsQuery'
 
 const PostList: React.FC = () => {
+  const { error, loading, data } = useQuery(GET_POSTS)
+
+  useEffect(() => {
+    console.log(data)
+  }, [data])
+
   return (
     <div className="post-list-container">
       <div className="post-list-items">
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
         <Post />
       </div>
     </div>
